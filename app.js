da06bd24a08962460144fec9f1925030f2b04574
Tyson Hofstetter <tysonh652@gmail.com>
2019-01-16 11:14:11 +1100

    copy stuff from old repo

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const morgan = require("morgan");
+const app = express();
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+app.use(morgan("combined"));
+
+app.use(require("./routes"));
+
+app.use(require("./middleware/error_handler_middleware"));
+
+module.exports = app;
