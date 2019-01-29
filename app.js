0338c0499ffe3467fe16f0288f801c6581675666
Theodore Chuang <chuangtheodore@gmail.com>
2019-01-29 23:18:06 +1100

    Change env variables for production

--- app.js
+++ app.js
@@ -8,8 +8,8 @@ app.use(passport.initialize());
 
 app.use(
   cors({
-    // origin: process.env.PROD_FRONT_END_DOMAIN
-    origin: process.env.FRONT_END_DOMAIN
+    origin: process.env.PROD_FRONT_END_DOMAIN
+    // origin: process.env.FRONT_END_DOMAIN
   })
 );
 
