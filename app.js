c1072f2075c917104d0fd6ace03ad52d5124ab05
Theodore Chuang <chuangtheodore@gmail.com>
2019-01-30 12:11:47 +1100

    CORS open Origin, block when setting specific URL

--- app.js
+++ app.js
@@ -24,12 +24,10 @@ app.use(passport.initialize());
 // app.use(cors(corsOptions));
 
 app.use(function(req, res, next) {
-  res.header(
-    "Access-Control-Allow-Origin",
-    `${process.env.PROD_FRONT_END_DOMAIN}`
-  );
   // Instead of "*" you should enable only specific origins
-  // res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Origin", "*");
+  // FIXME: URL locked by CORS policy: Response to preflight request doesn't pass access control check
+  // res.header("Access-Control-Allow-Origin", `${process.env.PROD_FRONT_END_DOMAIN}`);
   // Supported HTTP verbs
   res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
   // Other custom headers
