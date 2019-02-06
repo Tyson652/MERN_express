5c541f77c87d7271358d4c9f7ec6bcf7a7240b97
Theodore Chuang <chuangtheodore@gmail.com>
2019-02-07 00:18:44 +1100

    Restrict CORS to front-end only

--- app.js
+++ app.js
@@ -1,24 +1,26 @@
 const express = require("express");
-const morgan = require("morgan");
 const app = express();
+const cors = require("cors");
 const passport = require("./config/passport");
+const morgan = require("morgan");
+
+var whitelist = [
+  `${process.env.FRONT_END_DOMAIN}`,
+  `${process.env.PROD_FRONT_END_DOMAIN}`
+];
+var corsOptions = {
+  origin: function(origin, callback) {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  }
+};
+
+app.use(cors(corsOptions));
 
 app.use(passport.initialize());
-
-app.use(function(req, res, next) {
-  // Instead of "*" you should enable only specific origins
-  res.header("Access-Control-Allow-Origin", "*");
-  // FIXME: URL locked by CORS policy: Response to preflight request doesn't pass access control check
-  // res.header("Access-Control-Allow-Origin", `${process.env.PROD_FRONT_END_DOMAIN}`);
-  // Supported HTTP verbs
-  res.header("Access-Control-Allow-Methods", "GET, PUT, PATCH, POST, DELETE");
-  // Other custom headers
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Authorization, Content-Length, Content-Type, Accept"
-  );
-  next();
-});
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
